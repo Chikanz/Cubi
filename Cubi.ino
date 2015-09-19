2fe7d07ec2980a790db2935bf072bb8cd5715646
Zac Lucarelli <chikanz@outlook.com>
2015-09-19 17:32:57 +1000

    Added a photoshoot state that cycles through good looking times with the knob adjusting brightness

--- Cubi.ino
+++ Cubi.ino
@@ -166,7 +166,8 @@ enum eState
 	Alarm,			// 6
 	ChangeColor,	// 7
 	DisplayTest,	// 8
-	Snake			// 9
+	Snake,			// 9
+	StaticDisplay,	//10
 };
 
 //
@@ -291,6 +292,8 @@ bool servoPos = false;
 
 bool hourBelow10;
 bool canTest = true;
+
+int fakeTime = 0;
 
 
 void loop()
@@ -598,6 +601,42 @@ break;
 	if (State == Snake)
 	{
 		snake();
+	}
+
+	if (State == StaticDisplay)
+	{
+		targetBrightness = ReadRotary(targetBrightness, 0, 30);
+
+		if (buttonPressed())
+			fakeTime += 1;
+		
+
+		if (fakeTime > 4)
+			fakeTime = 0;
+
+		switch (fakeTime)
+		{
+		case 0:
+			DisplayCurrentTime(0, 4, 2, 0, false, 0);
+			break;
+
+		case 1:
+			DisplayCurrentTime(1, 1, 3, 7, false, 0);
+			break;
+
+		case 2:
+			DisplayCurrentTime(1, 3, 3, 7, false, 0);
+			break;
+
+		case 3:
+			DisplayCurrentTime(0, 2, 4, 7, false, 0);
+			break;
+
+		case 4:
+			DisplayCurrentTime(1, 2, 3, 1, false, 0);
+			break;
+
+		}
 	}
 
 	//Display Changes
