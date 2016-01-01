3d1cb418f2a029c1d56b011143a6676d2d7fa4d0
Zac Lucarelli <chikanz@outlook.com>
2016-01-01 20:23:30 +1100

    Added states and efontSize

--- Classes.h
+++ Classes.h
@@ -1,6 +1,58 @@
 // Classes.h
+#ifndef CLASSES_H
+#define CLASSES_H
+#include "Adafruit_GFX.h"
+#include "Adafruit_NeoMatrix.h"
+#include "Adafruit_NeoPixel.h"
+
 struct HrMn
 	{
 		int Hr;
 		int Mn;
 	};
+
+enum efontSize
+{
+	Small,
+	Med,
+	Big
+};
+
+enum eState
+{
+	Main,
+	Alarm,
+
+	AlarmMenu,
+
+	Brightness,
+	BrightnessProfile,
+
+	Snake,
+	SnakeGameOver,
+
+	TimeSetMode,
+	NapSet,
+	SetAlarm,
+	ChangeColor,
+
+	DisplayTest,
+	StaticDisplay,
+};
+
+class NumConveyor
+{
+	int belt, target;
+	int ymod = 0;
+	int speed;
+
+	uint16_t col;
+
+	long timer;
+
+	int velocity;
+
+public:
+	void Update(int numTarget, int x, int speed, uint16_t colour, efontSize size);
+};
+#endif
