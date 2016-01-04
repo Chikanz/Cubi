e80723998635ecee7cf148ab2c4b6435c9ac67a3
Zac Lucarelli <chikanz@outlook.com>
2016-01-04 20:11:02 +1100

    Made brightness return a value instead of being void

--- Brightness.ino
+++ Brightness.ino
@@ -6,37 +6,58 @@ int brightnessLevels[] =
 	20,
 	10,
 	5,
-	1,
+	2,
 	1,
 	0,
 };
 uint16_t dotCol = colors[displayCol2];
 
-void BrightnessSelect()
-{
-	pirrana(-2, 0, 500);
+int BrightnessReturn(bool setNow)
+{	
+	int prevbright = targetBrightness;
+	int bright;
 
-	if (brightnessGuage >= 0 && brightnessGuage <= 7)
-		brightnessGuage = ReadRotary(brightnessGuage);
-	else
-		brightnessGuage = force(brightnessGuage, 0, 7);
-
-
-	matrix.drawLine(0, 0, 0, 8, colors[displayCol2]);
-
-	if (brightnessGuage == 7)
+	//TODO: Find the set brightness and set it to brightnessGuage
+	while (1 < 2)
 	{
 		matrix.fillScreen(0);
+
+		pirrana(-2, 0, 500);
+
+		if (brightnessGuage >= 0 && brightnessGuage <= 7)
+			brightnessGuage = ReadRotary(brightnessGuage);
+		else
+			brightnessGuage = force(brightnessGuage, 0, 7);
+
+		matrix.drawLine(0, 0, 0, 8, colors[displayCol2]);
+		matrix.drawPixel(0, brightnessGuage, dotCol);
+
+		if (brightnessGuage == 7)
+			matrix.fillScreen(0);
+
+		targetBrightness = brightnessLevels[brightnessGuage];
+		bright = brightnessLevels[brightnessGuage];
+		SetLight();
+
+		if (brightnessGuage == 6)
+			RedColours();
+		else
+			unRedColours();
+
+		if (buttonPressed())
+		{
+			
+			if(!setNow)
+				targetBrightness = prevbright;
+
+			brightnessGuage = 2;
+			return bright;
+		}
+
+		if (digitalRead(2) == LOW)
+			canPress = true;
+
+		delay(20);
+		matrix.show();
 	}
-
-	matrix.drawPixel(0, brightnessGuage, dotCol);
-
-	targetBrightness = brightnessLevels[brightnessGuage];
-
-	if (brightnessGuage == 6)
-		RedColours();
-	else
-		unRedColours();
-
-	delay(20);
 }
