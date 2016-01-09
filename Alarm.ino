caa9e26d2e72b0763fca15223021410abb0b6234
Zac Lucarelli <chikanz@outlook.com>
2016-01-09 19:25:09 +1100

    added per day alarm (not stable)

--- Alarm.ino
+++ Alarm.ino
@@ -78,7 +78,6 @@ void AlarmPageChange()
 		break;
 
 	case 1:
-		Serial.println("Setting Alarm");
 		State = SetAlarm;
 		AlarmisSet = true;
 		break;
@@ -99,4 +98,118 @@ void AlarmSet()
 	//AlarmisSet = true;
 	//matrix.drawPixel (0, 0, Red);
 	oke();
+}
+
+void perDayAlarm()
+{
+	//Per day alarm
+
+	//Force the cursor pos and get input
+	if (cursorPos >= 0 && cursorPos <= 7)
+		cursorPos = ReadRotary(cursorPos, true);
+	else
+		cursorPos = force(cursorPos, 0, 7);
+
+	//Button press
+	if (buttonPressed())
+	{
+		if (cursorPos == 7)
+		{
+			EEPROM.put(10 + sizeof(BProfile), Alarms);
+			EEPROM.write(5, true);
+			cursorPos = 0;
+			oke(true);
+		}
+		else
+		{
+			if (Alarms[cursorPos].active)
+			{
+				Alarms[cursorPos].active = false;
+				Alarms[cursorPos].time = { 0,0 };
+			}
+			else
+			{
+				Alarms[cursorPos].active = true;
+				Alarms[cursorPos].time = TimeSetReturn(false);
+				oke(false);
+			}
+		}
+	}
+
+	//Display above selection
+	if (cursorPos == 7)
+		backIcon(0, 0, colors[displayCol2], false);
+	else if (Alarms[cursorPos].active)
+		displayTimeSimple(Alarms[cursorPos].time, Med, false, false);
+	else if (!Alarms[cursorPos].active)
+		switch (cursorPos) //TODO: replace with something scrolly someday
+		{
+		case 0:
+			sunday(1, 2, colors[displayCol1], colors[displayCol2]);
+			break;
+		case 1:
+			monday(1, 2, colors[displayCol1], colors[displayCol2]);
+			break;
+		case 2:
+			tuesday(1, 2, colors[displayCol1], colors[displayCol2]);
+			break;
+		case 3:
+			wednesday(1, 2, colors[displayCol1], colors[displayCol2]);
+			break;
+		case 4:
+			thursday(1, 2, colors[displayCol1], colors[displayCol2]);
+			break;
+		case 5:
+			friday(1, 2, colors[displayCol1], colors[displayCol2]);
+			break;
+		case 6:
+			saturday(1, 2, colors[displayCol1], colors[displayCol2]);
+			break;
+		}
+
+	//Draw Pixels
+	for (int i = 0; i < 7; i++)
+	{
+		if (i == cursorPos)
+		{
+			if (Alarms[i].active)
+				matrix.drawPixel(i, 7, colors[1] - fadeMod);
+			else
+				matrix.drawPixel(i, 7, colors[6] - fadeMod);
+		}
+		else
+		{
+			if (Alarms[i].active)
+				matrix.drawPixel(i, 7, colors[1]);
+			else
+				matrix.drawPixel(i, 7, colors[6]);
+		}
+	}
+	matrix.drawPixel(7, 7, colors[displayCol1]);
+
+	//Create a blink effect
+	//if (onHalfSecond())
+	//matrix.drawPixel(cursorPos, 7, matrix.Color(0, 0, 0));
+	cursorFadeTimer += 51;
+
+	if (cursorFadeTimer > 100)
+	{
+		if (fadeDirection)
+			fadeMod += 2;
+		else
+			fadeMod -= 1;
+
+		cursorFadeTimer = 0;
+	}
+
+	//if (fadeMod <= 100)
+	//	fadeDirection = true;
+
+	if (fadeMod >= 120)
+		fadeMod = 100;
+	//fadeDirection = false;
+
+	Serial.println(fadeMod);
+
+	delay(50);
 }
